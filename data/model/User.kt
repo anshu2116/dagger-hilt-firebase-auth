2656ffd1236192eb498bd3b26414a75fd2729fae
Anshu <104880008+anshu2116@users.noreply.github.com>
2025-08-01 11:51:57 +0530

    Delete data directory

--- data/model/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.firebaseauthapp.data.model
-
-data class User(
-    val uid: String = "",
-    val email: String = "",
-    val name: String = ""
-)
--- data/repository/AuthRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.firebaseauthapp.data.repository
-
-import com.google.firebase.auth.FirebaseUser
-
-interface AuthRepository {
-    suspend fun login(email: String, password: String): Result<FirebaseUser?>
-    suspend fun signup(name: String, email: String, password: String): Result<FirebaseUser?>
-}
